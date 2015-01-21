007186fc4bd70f3b3015f995f7b724478e0d6e76
vagrant <vagrant@precise32.(none)>
2015-01-21 20:34:37 +0000

    added my javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,15 +4,60 @@
 $(document).ready(function() {
 	initializePage();
 })
-
+var addOrRemove = 1;
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		//$("#testjs").text("Please wait...");
+		
+		if ( addOrRemove == 1) {
+ 			 $( ".jumbotron p" ).addClass( "active" );
+ 			 $("#testjs").text("Please wait...");
+ 			 addOrRemove = 0;
+		} else {
+ 			 $( ".jumbotron p" ).removeClass( "active" );
+ 			 $("#testjs").text("TestJavascript!");
+ 			 addOrRemove = 1;
+		}
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject); 
+}
+
+function projectClick(e) { 
+	//console.log("Project clicked");
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    //$(this).css("background-color", "#7fff00");
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else if($(".project-description").is(":hidden") == true){ 
+       $(".project-description").show();
+    } else { 
+       $(".project-description").hide();
+    }
+
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
